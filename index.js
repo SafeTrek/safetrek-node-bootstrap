74cd0fe54376eb1aa4fd5cdf00d89aef37a63042
Sean Mishra <sean_mishra@safetrekapp.com>
2018-03-16 15:15:12 -0500

    Refactored to Pug and added some UX

--- index.js
+++ index.js
@@ -11,8 +11,32 @@ const port = env.PORT || 5000
  * Values provided at the time of Heroku deployment will be used by default.
  */
 
-// SafeTrek OAuth token URL (Default: https://login-sandbox.safetrek.io/oauth/token).
+/**
+ * SafeTrek OAuth account authorization URL
+ * Production: https://account.safetrek.io
+ * Sandbox: https://account-sandbox.safetrek.io
+ */
+const AUTH_URL = 'https://account-sandbox.safetrek.io'
+
+/**
+ * SafeTrek OAuth token URL
+ * Production: https://login.safetrek.io/oauth/token
+ * Sandbox: https://login-sandbox.safetrek.io/oauth/token
+ */
 const TOKEN_URL = 'https://login-sandbox.safetrek.io/oauth/token'
+
+/**
+ * SafeTrek API base URL
+ * Production: https://api.safetrek.io
+ * Sandbox: https://api-sandbox.safetrek.io
+ */
+const API_URL = 'https://api-sandbox.safetrek.io'
+
+// Default scope. DO NOT ALTER.
+const SCOPE = 'openid phone offline_access'
+
+// Default response type. DO NOT ALTER.
+const RESPONSE_TYPE = 'code'
 
 // Enter your SafeTrek client id.
 const CLIENT_ID = ''
@@ -22,16 +46,16 @@ const CLIENT_SECRET = ''
 
 // Enter where you want to redirect after retrieving your 'access_token' and 'refresh_token'.
 // For debugging, you can set this to be a RequestBin URL from https://requestb.in
-const REDIRECT_URL = '' || env.REDIRECT_URL
+const REDIRECT_URL = ''
 
 // OAuth demo URL. Will be used as REDIRECT_URL if none is provided.
-const DEMO_URL = ''
+const DEMO_URL = '/demo'
 
 
 // Middleware to filter all but GET method requests
 app.use((req, res, next) => {
   if(req.method !== 'GET') {
-    res.status(400).send('Bad Request. Only GET requests are acceptable.')
+    res.status(405).send('Method not Allowed. Only GET requests are acceptable.')
   } else {
     next()
   }
@@ -41,30 +65,48 @@ app.use((req, res, next) => {
 // Enabling this ensures reporting of proper protocol.
 app.enable('trust proxy')
 
-app.use(express.static('./public/'))
+// Enabling Pug templating
+app.set('views', './views')
+app.set('view engine', 'pug')
 
+// Middleware to parse JSON data in requests
 app.use(express.json())
 
-app.get('/', function (req, res) {
-  let appUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-  let redirectUrl = REDIRECT_URL || DEMO_URL
-  unirest.post(TOKEN_URL)
-    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-    .send({
-      'grant_type': 'authorization_code',
-      'code': req.query.code,
-      'client_id': CLIENT_ID || env.CLIENT_ID,
-      'client_secret': CLIENT_SECRET || env.CLIENT_SECRET,
-      'redirect_uri': appUrl
-    })
-    .end((response) => {
-      log(response.body)
-      if(response.body.access_token && response.body.refresh_token && redirectUrl) {
-        res.redirect(`${redirectUrl}?access_token=${response.body.access_token}&refresh_token=${response.body.refresh_token}`)
-      } else {
-        res.status(500).send('Internal Server Error. Something went wrong. Please try again')
-      }
-    })
+app.get('/demo', function (req, res) {
+  let appUrl = `${req.protocol}://${req.get('host')}`
+  let client_id = CLIENT_ID || env.CLIENT_ID || ''
+  res.render('index', {
+    auth_url: `${AUTH_URL}/authorize?audience=${API_URL}&client_id=${client_id}&scope=${SCOPE}&response_type=${RESPONSE_TYPE}&redirect_uri=${appUrl}`
+  })
 })
+
+app.get('/', function (req, res) {
+  if(!req.query.code) {
+    res.status(422).send('Unprocessable Entity. A required parameter is missing.')
+  } else {
+    let appUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+    let redirectUrl = REDIRECT_URL || env.REDIRECT_URL || DEMO_URL
+    unirest.post(TOKEN_URL)
+      .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+      .send({
+        'grant_type': 'authorization_code',
+        'code': req.query.code,
+        'client_id': CLIENT_ID || env.CLIENT_ID || '',
+        'client_secret': CLIENT_SECRET || env.CLIENT_SECRET || '',
+        'redirect_uri': appUrl
+      })
+      .end((response) => {
+        log(response.body)
+        if(response.body.access_token && response.body.refresh_token && redirectUrl) {
+          res.redirect(`${redirectUrl}?authorization_code=${req.query.code}&access_token=${response.body.access_token}&refresh_token=${response.body.refresh_token}`)
+        } else {
+          res.status(500).send('Internal Server Error. Something went wrong. Please try again')
+        }
+      })
+  }
+})
+
+// Fallback to public for loading assets
+app.use(express.static('./public/'))
 
 app.listen(port, () => { log(`Listening on port ${port}`) })
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,54 @@
 $(function(){
+
+  const ls = localStorage
+  const log = console.log
+  let state = new Map
+  const setState = (key, val) => {
+    ls.setItem(key, val)
+    state.set(key, val)
+  }
+
+  // Function to fetch URL parameters
+  const urlParam = (name) => {
+    let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href)
+    return results ? decodeURIComponent(results[1]) : 0
+  }
+
+  // State Initialization
+  state.set('status', 'disconnected')
+  state.set('authorization_code', ls.getItem('authorization_code') || '')
+  state.set('access_token', ls.getItem('access_token') || '')
+  state.set('refresh_token', ls.getItem('refresh_token') || '')
+
+  // Materialize Components Initialization
   $('.button-collapse').sideNav()
+
+  // Update state based on query params
+  if(urlParam('authorization_code') && urlParam('access_token') && urlParam('refresh_token')) {
+    setState('authorization_code', urlParam('authorization_code'))
+    setState('access_token', urlParam('access_token'))
+    setState('refresh_token', urlParam('refresh_token'))
+  }
+
+  if(state.get('authorization_code')) {
+    state.set('status', 'connected')
+    log('SafeTrek is connected!', state)
+    $('a.safetrek-btn > img').attr('src', '/assets/images/safetrek-disconnect-button.png')
+    $('input#authorization_code').val(state.get('authorization_code'))
+  }
+
+  $('input.display-only').on('blur', function() {
+    $(this).val(state.get($(this).attr('id')))
+  })
+
+  $('a.safetrek-btn').on('click', function(e){
+    if(state.get('status') === 'connected') {
+      e.preventDefault()
+      ls.clear()
+      location.href = location.origin + location.pathname
+    }
+  })
+
+
+
 })
