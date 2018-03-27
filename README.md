4263430b78f2dd0042f0e6567576a8ed246ff6fd
Sean Mishra <sean_mishra@safetrekapp.com>
2018-03-27 13:34:46 -0500

    Updated README and fixed BrowserSync issue

--- README.md
+++ README.md
@@ -1,29 +1,56 @@
+
 # SafeTrek Node Bootstrap
 
-A bootstrap project to quickly get you started with the SafeTrek API using NodeJS.
+A bootstrap project to get you started with the SafeTrek API using NodeJS.
 
-### How to use
+## Get Started
+To get started with the SafeTrek API, you’ll need to do three things:
+- Create or access your [Heroku](https://www.heroku.com/) account.
 
- - **Step 1:** [Request Access](https://developers.safetrek.io/) to SafeTrek API in order to get your `client_id` and `client_secret`
- - **Step 2:** [Create](https://www.heroku.com/) a free Heroku account, if you don't have one already.
- - **Step 3:** Use the *Deploy to Heroku* button below to create your new app off of this repository. Give your app a name and fill in your SafeTrek API credentials that you received from *Step 1*. Fill in the `REDIRECT_URL` only if you know what you're doing. If this is your first time using this repo, leave it blank.
-<p align="center">
- <a href='https://heroku.com/deploy?template=https://github.com/SafeTrek/safetrek-node-bootstrap'>
-  <img src='https://www.herokucdn.com/deploy/button.svg' alt='Deploy'>
- </a>
-</p>
+- [Request access](https://developers.safetrek.io/request-access) to get your `client_id` and `client_secret` from SafeTrek.
 
-- **Step 4:** Open up the app and go through the demo if you're new to working with SafeTrek API. The console outputs can give additional information during the demo, so keep an eye out for that.
-- **Step 5:** Go through the code to better understand how everything works.
-- **Step 6:** Fork this repo to your own GitHub account.
-- **Step 7:** Connect the forked repo to your Heroku app from the **Deploy** menu on Heroku's dashboard. (`https://dashboard.heroku.com/apps/<your-app>/deploy/github`)
-- **Step 8:** Now you can deploy any branch from your forked repo to your Heroku app. You can even setup auto-deploy if you want. All that's left to do is cloning your forked repo and pushing some changes!
+- Use the _Deploy to Heroku_ button below to create your new app off of this repository.
+	- Give your app a name and fill in your SafeTrek API credentials that you received.
+	- If this is your first time using this repo, leave the `REDIRECT_URL` blank.
 
-If you have any queries, suggestions or bug reports, feel free to [create an issue](https://github.com/SafeTrek/safetrek-node-bootstrap/issues/new).
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/SafeTrek/safetrek-node-bootstrap)
 
-Good Luck!
+## Integrate  
+A few more quick steps to get you up and going:
 
-### More Info
+- Once deployed, go through the demo experience to better understand how to integrate SafeTrek into your app so that you can provide peace of mind to your users.
 
+  - **TIP:** Be sure to keep an eye out for the console outputs during the demo.
+
+- When you’ve gone through the code and gotten the hang of it, fork this repo to your own GitHub account.
+
+- Connect the forked repo to your Heroku app from the **Deploy** menu on Heroku's dashboard.
+
+  -  `https://dashboard.heroku.com/apps/<YOUR-APP-NAME>/deploy/github`
+
+- Deploy any branch from your forked repo to your Heroku app. (You can even setup auto-deploy if you want 😎 ). Just clone your forked repo to push any changes.
+
+- For testing on `localhost`, create a file named `.env` in the root directory and set your SafeTrek API credentials as below (no quotes or spaces):
+
+```
+CLIENT_ID=<your-client-id>
+CLIENT_SECRET=<your-client-secret>
+```
+
+- Next, execute `npm install` followed by `npm start`
+
+What life-saving integration will you create?
+
+**Questions? Suggestions? Bugs? [Create an issue](https://github.com/SafeTrek/safetrek-node-bootstrap/issues/new) or [Email Us](mailto:developers@safetrekapp.com/?subject=[Query]%20SafeTrek%20Node%20Bootstrap).**
+
+## The Fine Print
 - Currently, contributions to this repository are not being accepted.
-- The `/callback` path on all `*.herokuapp.com` domain URLs have been whitelisted for making secure calls to the SafeTrek API. If you want to whitelist a custom path or domain, please contact us.
+
+- The `/callback` path on the following URLs have been whitelisted for making secure calls to the SafeTrek API.
+  - `https://*.herokuapp.com`
+  - `http://localhost:3000`
+  - `http://localhost:5000`
+  - `http://localhost:8000`
+  - `http://localhost:8080`
+
+- If you want to whitelist a custom path or domain, please contact us.
--- public/scripts/src/main.js
+++ public/scripts/src/main.js
@@ -4,6 +4,10 @@ import 'materialize-css'
 
 $(function(){
 
+  // Set to 'true' to enforce BrowserSync proxy port redirection
+  const PROXY_SWITCH = true
+  const PROXY_PORT = 3000
+
   const CALLBACK_URL =  '/callback'
   const SAFETREK_API_URL =  'https://api-sandbox.safetrek.io/v1'
   const DEFAULT_ACCURACY =  5
@@ -20,179 +24,186 @@ $(function(){
     if (verbose) log('State changed!', `${key} has new value. Current State:\n`, state)
   }
 
-  // Function to fetch URL parameters
-  const urlParam = (name, url = window.location.href) => {
-    let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(url)
-    return results ? decodeURIComponent(results[1]) : 0
-  }
-
-  // State Initialization
-  state.set('status', 'disconnected')
-  state.set('authorization_code', ls.getItem('authorization_code') || '')
-  state.set('refresh_token', ls.getItem('refresh_token') || '')
-  state.set('access_token', ls.getItem('access_token') || '')
-
-  // Materialize Components Initialization
-  $('.button-collapse').sideNav()
-
-  // Update state based on query params
-  if(urlParam('authorization_code') && urlParam('access_token') && urlParam('refresh_token')) {
-    setState('authorization_code', urlParam('authorization_code'))
-    setState('access_token', urlParam('access_token'))
-    setState('refresh_token', urlParam('refresh_token'))
-  }
-
-  if(state.get('authorization_code')) {
-    state.set('status', 'connected')
-    let btn = $('a.safetrek-btn > img')
-    let discBtnImg = $('a.safetrek-btn > img').attr('data-disc-src')
-    log('SafeTrek is connected! Current State:', state)
-    btn.attr('src', discBtnImg)
-    $('input#authorization_code').val(state.get('authorization_code'))
-    $('input#access_token').val(state.get('access_token'))
-    $('input#refresh_token').val(state.get('refresh_token'))
+  // Redirect to browser-sync proxy port
+  if(location.port == 5000 && PROXY_SWITCH) {
+    location.port = PROXY_PORT
   } else {
-    logWarn('SafeTrek is not connected! Current State:\n', state)
-  }
 
-  // Prevent changing code and token field values
-  $('input.display-only').on('blur', function() {
-    $(this).val(state.get($(this).attr('id')))
-  })
+    // Function to fetch URL parameters
+    const urlParam = (name, url = window.location.href) => {
+      let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(url)
+      return results ? decodeURIComponent(results[1]) : 0
+    }
 
-  // Disconnect from SafeTrek. Clear all data and reload page.
-  $('a.safetrek-btn').on('click', function(e){
-    e.preventDefault()
-    let that = $(this)
-    if(state.get('status') !== 'disconnected') {
-      ls.clear()
-      location.href = location.origin + location.pathname
+    // State Initialization
+    state.set('status', 'disconnected')
+    state.set('authorization_code', ls.getItem('authorization_code') || '')
+    state.set('refresh_token', ls.getItem('refresh_token') || '')
+    state.set('access_token', ls.getItem('access_token') || '')
+
+    // Materialize Components Initialization
+    $('.button-collapse').sideNav()
+
+    // Update state based on query params
+    if(urlParam('authorization_code') && urlParam('access_token') && urlParam('refresh_token')) {
+      setState('authorization_code', urlParam('authorization_code'))
+      setState('access_token', urlParam('access_token'))
+      setState('refresh_token', urlParam('refresh_token'))
+    }
+
+    if(state.get('authorization_code')) {
+      state.set('status', 'connected')
+      let btn = $('a.safetrek-btn > img')
+      let discBtnImg = $('a.safetrek-btn > img').attr('data-disc-src')
+      log('SafeTrek is connected! Current State:', state)
+      btn.attr('src', discBtnImg)
+      $('input#authorization_code').val(state.get('authorization_code'))
+      $('input#access_token').val(state.get('access_token'))
+      $('input#refresh_token').val(state.get('refresh_token'))
     } else {
-      let url = ''
-      if(that.attr('data-localhost') === 'true') {
-        url = that.attr('data-href')
-        url += that.attr('data-protocol')
-        url += '://localhost:'
-        url += that.attr('data-port')
-        url += that.attr('data-callback')
-      } else {
-        url = that.attr('data-href')
-        url += that.attr('data-redirect')
-      }
-      location.href = url
+      logWarn('SafeTrek is not connected! Current State:\n', state)
     }
-  })
 
-  // Exchange refresh_token for new access_token
-  $('.new-token').on('click', function() {
-    let that = $(this)
-    that.prop('disabled', true)
-    let url = `${CALLBACK_URL}?refresh_token=${state.get('refresh_token')}`
-    $.ajax({
-      url: url,
-      dataType: 'json',
-      success: (data) => {
-        setState('access_token', data.access_token, true)
-        $('input#access_token').val(data.access_token)
-      },
-      error: (xhr, status, err) => { logErr('Error:', err) },
-      complete: () => { that.prop('disabled', false) }
+    // Prevent changing code and token field values
+    $('input.display-only').on('blur', function() {
+      $(this).val(state.get($(this).attr('id')))
     })
-  })
 
-  $('a.make-alarm-request').on('click', function(e) {
-    e.preventDefault()
-    if (state.get('status') === 'active-alarm') {
-      log('Alarm status is currently active and will reset in 10s or less.')
-    } else if(state.get('status') !== 'processing') {
-      if(state.get('access_token')) {
-        $('.alarm').removeClass('alarm-red')
-        $('.alarm-status').text('Requesting...')
-        state.set('status', 'processing')
-        let url = SAFETREK_API_URL + '/alarms'
-        let data = $('code.alarm-request').text()
-        log('Requesting Alarm creation with data:\n', data)
-        $.ajax({
-          url: url,
-          type: 'post',
-          headers: {
-            'Authorization': `Bearer ${state.get('access_token')}`
+    // Disconnect from SafeTrek. Clear all data and reload page.
+    $('a.safetrek-btn').on('click', function(e){
+      e.preventDefault()
+      let that = $(this)
+      if(state.get('status') !== 'disconnected') {
+        ls.clear()
+        location.href = location.origin + location.pathname
+      } else {
+        let url = ''
+        if(that.attr('data-localhost') === 'true') {
+          url = that.attr('data-href')
+          url += that.attr('data-protocol')
+          url += '://localhost:'
+          url += that.attr('data-port')
+          url += that.attr('data-callback')
+        } else {
+          url = that.attr('data-href')
+          url += that.attr('data-redirect')
+        }
+        location.href = url
+      }
+    })
+
+    // Exchange refresh_token for new access_token
+    $('.new-token').on('click', function() {
+      let that = $(this)
+      that.prop('disabled', true)
+      let url = `${CALLBACK_URL}?refresh_token=${state.get('refresh_token')}`
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        success: (data) => {
+          setState('access_token', data.access_token, true)
+          $('input#access_token').val(data.access_token)
+        },
+        error: (xhr, status, err) => { logErr('Error:', err) },
+        complete: () => { that.prop('disabled', false) }
+      })
+    })
+
+    $('a.make-alarm-request').on('click', function(e) {
+      e.preventDefault()
+      if (state.get('status') === 'active-alarm') {
+        log('Alarm status is currently active and will reset in 10s or less.')
+      } else if(state.get('status') !== 'processing') {
+        if(state.get('access_token')) {
+          $('.alarm').removeClass('alarm-red')
+          $('.alarm-status').text('Requesting...')
+          state.set('status', 'processing')
+          let url = SAFETREK_API_URL + '/alarms'
+          let data = $('code.alarm-request').text()
+          log('Requesting Alarm creation with data:\n', data)
+          $.ajax({
+            url: url,
+            type: 'post',
+            headers: {
+              'Authorization': `Bearer ${state.get('access_token')}`
+            },
+            contentType: 'application/json',
+            dataType: 'json',
+            data: data,
+            success: (data) => {
+              log('Alarm created successfully! Server response:\n', JSON.stringify(data, null, 2), '\nAlarm status will reset in 10s.')
+              $('.alarm').addClass('alarm-red')
+              $('.alarm-status').text('Alarm created! Check console for JSON response.')
+            },
+            error: (xhr, status, err) => { logErr('Error:', err) },
+            complete: () => {
+              state.set('status', 'active-alarm')
+              setTimeout(() => {
+                state.set('status', 'connected')
+                $('.alarm').removeClass('alarm-red')
+                $('.alarm-status').text('')
+                log('Alarm status reset!')
+              }, 10000)
+            }
+          })
+        } else {
+          logErr('No valid access_token found! Connect to SafeTrek before requesting Alarm creation.')
+        }
+      }
+    })
+
+    $('.use-addr').on('click', function(e) {
+      e.preventDefault()
+      let that = $(this)
+      $.getJSON(RANDOM_ADDRESS_DATA, (data) => {
+        const addresses = data.addresses
+        const randomAddress = addresses[Math.floor(Math.random() * addresses.length)]
+        let responseJSON = {
+          "services": {
+            "police": true,
+            "fire": false,
+            "medical": false
           },
-          contentType: 'application/json',
-          dataType: 'json',
-          data: data,
-          success: (data) => {
-            log('Alarm created successfully! Server response:\n', JSON.stringify(data, null, 2), '\nAlarm status will reset in 10s.')
-            $('.alarm').addClass('alarm-red')
-            $('.alarm-status').text('Alarm created! Check console for JSON response.')
-          },
-          error: (xhr, status, err) => { logErr('Error:', err) },
-          complete: () => {
-            state.set('status', 'active-alarm')
-            setTimeout(() => {
-              state.set('status', 'connected')
-              $('.alarm').removeClass('alarm-red')
-              $('.alarm-status').text('')
-              log('Alarm status reset!')
-            }, 10000)
+          "location.address": {
+            "line1": randomAddress.address1,
+            "line2": randomAddress.address2,
+            "city": randomAddress.city,
+            "state": randomAddress.state,
+            "zip": randomAddress.postalCode
           }
-        })
-      } else {
-        logErr('No valid access_token found! Connect to SafeTrek before requesting Alarm creation.')
-      }
-    }
-  })
-
-  $('.use-addr').on('click', function(e) {
-    e.preventDefault()
-    let that = $(this)
-    $.getJSON(RANDOM_ADDRESS_DATA, (data) => {
-      const addresses = data.addresses
-      const randomAddress = addresses[Math.floor(Math.random() * addresses.length)]
-      let responseJSON = {
-        "services": {
-          "police": true,
-          "fire": false,
-          "medical": false
-        },
-        "location.address": {
-          "line1": randomAddress.address1,
-          "line2": randomAddress.address2,
-          "city": randomAddress.city,
-          "state": randomAddress.state,
-          "zip": randomAddress.postalCode
         }
-      }
-      $('code.alarm-request').text(JSON.stringify(responseJSON, null, 2))
-      that.addClass('hide')
-      $('.use-coords').removeClass('hide')
-      log('Using random location address.')
+        $('code.alarm-request').text(JSON.stringify(responseJSON, null, 2))
+        that.addClass('hide')
+        $('.use-coords').removeClass('hide')
+        log('Using random location address.')
+      })
     })
-  })
 
-  $('.use-coords').on('click', function(e) {
-    e.preventDefault()
-    let that = $(this)
-    $.getJSON(RANDOM_ADDRESS_DATA, (data) => {
-      const addresses = data.addresses
-      const randomAddress = addresses[Math.floor(Math.random() * addresses.length)]
-      let responseJSON = {
-        "services": {
-          "police": true,
-          "fire": false,
-          "medical": false
-        },
-        "location.coordinates": {
-          "lat": randomAddress.coordinates.lat,
-          "lng": randomAddress.coordinates.lng,
-          "accuracy": DEFAULT_ACCURACY
+    $('.use-coords').on('click', function(e) {
+      e.preventDefault()
+      let that = $(this)
+      $.getJSON(RANDOM_ADDRESS_DATA, (data) => {
+        const addresses = data.addresses
+        const randomAddress = addresses[Math.floor(Math.random() * addresses.length)]
+        let responseJSON = {
+          "services": {
+            "police": true,
+            "fire": false,
+            "medical": false
+          },
+          "location.coordinates": {
+            "lat": randomAddress.coordinates.lat,
+            "lng": randomAddress.coordinates.lng,
+            "accuracy": DEFAULT_ACCURACY
+          }
         }
-      }
-      $('code.alarm-request').text(JSON.stringify(responseJSON, null, 2))
-      that.addClass('hide')
-      $('.use-addr').removeClass('hide')
-      log('Using random location coordinates.')
+        $('code.alarm-request').text(JSON.stringify(responseJSON, null, 2))
+        that.addClass('hide')
+        $('.use-addr').removeClass('hide')
+        log('Using random location coordinates.')
+      })
     })
-  })
+
+  } //
 
 })
